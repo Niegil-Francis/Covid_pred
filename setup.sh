09abb3c7353b369a3e368c1fa56595db9f125b18
Niegil-Francis <niegilfrancis14@gmail.com>
2020-10-21 13:37:23 +0530

    Second

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 mkdir -p ~/.streamlit/
-echo “\
+
+echo "\
 [general]\n\
-email = \”your-email@domain.com\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
+email = \"niegilfrancis14@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
